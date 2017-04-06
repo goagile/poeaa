ea16374bdc4b298e6cef0cc321927e3fd9c5b575
kharchilin_di <kharchilin_di@rtsoft.msk.ru>
2017-04-06 12:15:06 +0300

    map

--- src/domain/Person.java
+++ src/domain/Person.java
@@ -3,4 +3,10 @@ package domain;
 
 public class Person {
 
+    public String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
 }
--- src/identity/map/PersonIdentityMap.java
+++ src/identity/map/PersonIdentityMap.java
@@ -1,8 +1,24 @@
 package identity.map;
 
 
-public class PersonIdentityMap {
-    public static void main(String... args) {
+import domain.Person;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class PersonIdentityMap {
+    private Map<Integer, Person> persons = new HashMap<>();
+
+    public static void main(String... args) {
+        PersonIdentityMap map = new PersonIdentityMap();
+        map.addPerson(123, new Person("Ilia"));
+        for(Person p : map.persons.values()) {
+            System.out.println(p.name);
+        }
+    }
+
+    public void addPerson(Integer key, Person p) {
+        this.persons.put(key, p);
     }
 }
