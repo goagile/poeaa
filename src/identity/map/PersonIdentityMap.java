c98278ae94915971e9726112fdb5fde52b10cf9f
kharchilin_di <kharchilin_di@rtsoft.msk.ru>
2017-04-06 12:01:58 +0300

    init project

---
+++ src/identity/map/PersonIdentityMap.java
@@ -0,0 +1,8 @@
+package identity.map;
+
+
+public class PersonIdentityMap {
+    public static void main(String... args) {
+
+    }
+}
---
+++ src/unit/of/work/PersonUnitOfWork.java
@@ -0,0 +1,7 @@
+package unit.of.work;
+
+
+public class PersonUnitOfWork {
+
+}
+
