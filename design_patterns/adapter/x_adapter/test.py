0df4ce09a8aecd9150b87825e6cbfff200aa3bf9
khardi <kharchilindanila@gmail.com>
2017-07-24 09:47:11 +0300

    adapter pass

--- design_patterns/adapter/x_adapter/test.py
+++ design_patterns/adapter/x_adapter/test.py
@@ -5,20 +5,22 @@ class Test(unittest.TestCase):
 
     def test_real_product(self):
         """ Стандартный интерфейс класса продукт """
-        p = Product(name='SkateBoard', price=10)
+        expected = 100
+        p = Product(name='SkateBoard', price=100)
 
         result = p.price
 
-        self.assertEqual(10, result)
+        self.assertEqual(expected, result)
 
     def test_product_adapter(self):
         """ Адаптируем продукт к новому интерфейсу, использующему Каталог цен товаров """
+        expected = 100
         price_list = PriceList()
         adapter = ProductAdapter(price_list, name='SkateBoard')
 
         result = adapter.price
 
-        self.assertEqual(10, result)
+        self.assertEqual(expected, result)
 
 
 class Product:
@@ -42,3 +44,15 @@ class PriceList:
         return self.prices[name]
 
 
+class ProductAdapter:
+
+    def __init__(self, price_list, name):
+        self.__price_list = price_list
+        self.__name = name
+
+    @property
+    def price(self):
+        price = self.__price_list.get_price(self.__name)
+        product = Product(self.__name, price)
+        result = product.price
+        return result
