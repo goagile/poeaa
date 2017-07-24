94cf9739590ba50496889259f8d8ba1d0822b412
khardi <kharchilindanila@gmail.com>
2017-07-24 09:15:45 +0300

    add simple fail test

--- design_patterns/proxy/orders_proxy/test.py
+++ design_patterns/proxy/orders_proxy/test.py
@@ -1,1 +1,20 @@
 import unittest
+
+
+class Test(unittest.TestCase):
+
+    def test_real_product_get_price(self):
+        expected = 10
+        product = ProductImpl(product_id=1, name='Beatbox', price=10)
+
+        result = product.price
+
+        self.assertEqual(expected, result)
+
+    def test_proxy_product_get_price(self):
+        expected = 10
+        product = ProductProxy(product_id=1)
+
+        result = product.price
+
+        self.assertEqual(expected, result)
